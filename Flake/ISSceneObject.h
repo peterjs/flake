26700da43c7d1a24760c219aea901077df45dcd6
Peter Somlo <peter.somlo@gmail.com>
2013-09-12 21:27:53 +0200

    Added ISSceneObject class for basic scene graph

---
+++ Flake/ISSceneObject.h
@@ -0,0 +1,22 @@
+//
+//  ISSceneObject.h
+//  Flake
+//
+//  Created by Peter Somlo on 9/12/13.
+//  Copyright (c) 2013 Peter Somlo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ISSceneObject : NSObject
+
+@property (nonatomic) CGRect AABB;
+@property (nonatomic) CGPoint position;
+@property (nonatomic) CGSize size;
+
+@property (nonatomic, strong) void(^draw)(CGContextRef);
+
+- (void)renderInContext:(CGContextRef)context;
+- (BOOL)hit:(CGPoint)hitPosition;
+
+@end
---
+++ Flake/ISSceneObject.m
@@ -0,0 +1,91 @@
+//
+//  ISSceneObject.m
+//  Flake
+//
+//  Created by Peter Somlo on 9/12/13.
+//  Copyright (c) 2013 Peter Somlo. All rights reserved.
+//
+
+#import "ISSceneObject.h"
+#import "DDLog.h"
+
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+@implementation ISSceneObject
+
+@synthesize AABB = _AABB;
+@synthesize position = _position;
+@synthesize size = _size;
+@synthesize draw = _draw;
+
+- (CGRect)AABB {
+    CGAffineTransform transform = CGAffineTransformMakeTranslation(0.0, 0.0);
+    //    transform = CGAffineTransformTranslate(transform, self.position.x, self.position.y);
+    //    transform = CGAffineTransformRotate(transform, self.roofAngle);
+    //    transform = CGAffineTransformScale(transform, self.roofScale, self.roofScale);
+    
+    //        self.roofAABB = CGRectApplyAffineTransform(roofOriginal, transform);
+    
+    //    transform = CGAffineTransformRotate(transform, self.angle);
+    
+    CGRect originalRect = CGRectMake(self.position.x, self.position.y, self.size.width, self.size.height);
+    
+    return originalRect;
+    return CGRectApplyAffineTransform(originalRect, transform);
+}
+
+//render to offscreen buffer, to test intersection
+- (BOOL)hit:(CGPoint)hitPosition {
+    //render to a dummy canvas and find if the hit point is filled or not
+    //    CGContextRef testContext = CGContext
+    
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    //In iOS 4.0 and later, and OS X v10.6 and later, you can pass NULL if you want Quartz to allocate memory for the bitmap.
+    CGContextRef testContext = CGBitmapContextCreate(NULL, self.size.width, self.size.height, 8, 4*self.size.width, colorspace, kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedFirst);
+    
+    UInt8* data = CGBitmapContextGetData(testContext);
+    
+    UIGraphicsPushContext(testContext);
+    self.draw(testContext);
+    
+    //self.size.height-
+    
+    CGPoint coord = CGPointMake(hitPosition.x-self.position.x, self.size.height-(hitPosition.y-self.position.y));
+    DDLogVerbose(@"coord %f %f", coord.x, coord.y);
+    int alpha = 0;
+    if (data != NULL) {
+        int offset = 4*((self.size.width*round(coord.y))+round(coord.x));
+        alpha =  data[offset];
+        int red = data[offset+1];
+        int green = data[offset+2];
+        int blue = data[offset+3];
+        
+        DDLogVerbose(@"alpha: %i red: %i green: %i blue: %i", alpha, red, green, blue);
+        
+    }
+    
+    UIGraphicsPopContext();
+    BOOL hit = YES;
+    if (alpha == 0) hit = NO;
+    return hit;
+}
+
+- (void)renderInContext:(CGContextRef)context {
+    CGContextSaveGState(context);
+    
+    CGAffineTransform transform = CGAffineTransformMakeTranslation(0.0, 0.0);
+    transform = CGAffineTransformTranslate(transform, self.position.x, self.position.y);
+    //    transform = CGAffineTransformRotate(transform, self.roofAngle);
+    //    transform = CGAffineTransformScale(transform, self.roofScale, self.roofScale);
+    
+    //        self.roofAABB = CGRectApplyAffineTransform(roofOriginal, transform);
+    
+    //    transform = CGAffineTransformRotate(transform, self.angle);
+    
+    CGContextConcatCTM(context, transform);
+    self.draw(context);
+    
+    CGContextRestoreGState(context);
+}
+
+@end
